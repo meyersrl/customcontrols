fdedefec7b2be048455d9c938f36426031e9e9d0
meyersrl <36867565+meyersrl@users.noreply.github.com>
2018-09-13 12:49:40 -0700

    Cleaned up config param for MUN

--- Select2Cognos.js
+++ Select2Cognos.js
@@ -22,7 +22,7 @@ function formatElement(element) {
 	return $element;
 };
 
-var sMUNBase = '';
+//var sMUNBase = '';
 var sParamName = '';
 
 define(['jquery', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js'], function ($) {
@@ -31,9 +31,7 @@ define(['jquery', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/selec
 
 	CustomSelect2.prototype.draw = function (oControlHost) {
 		sParamName = oControlHost.configuration['paramName'];
-		//sMUNBase = oControlHost.configuration['sampleMUN'].split(".").slice(0, -1).join(".");
 		var oParameter = oControlHost.getParameter(sParamName.toString());
-		//var sMUN = this.m_oDataStore.getCellValue(0, 0).split('.').pop();
 		var sParameterValue = (oParameter && (oParameter.values.length > 0)) ? oParameter.values[0].use : "";
 		var sTextValue = sParameterValue.split(".").pop().replace(/^\[(.+)\]$/, '$1');
 		var nameDataSet = oControlHost.control.dataStores[0];
@@ -52,7 +50,7 @@ define(['jquery', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/selec
 			var nNextLevel = levelDataSet.getCellValue(((iRow < iRowCount - 1) ? iRow + 1 : iRow), 0);
 			//console.log('Current Element:' + sValue + ' Current Level:' + nLevel + ' Next Level:' + nNextLevel);
 			$('.js-example-basic-single')
-			.append($("<option level=" + nLevel + ((sValue == sTextValue) ? ' selected="selected"' : '') + ((nLevel < nNextLevel) ? ' nLevel=1' : ' nLevel=0') + "mun='" + sMun +"'></option>")
+			.append($("<option level=" + nLevel + ((sValue == sTextValue) ? ' selected="selected"' : '') + ((nLevel < nNextLevel) ? ' nLevel=1' : ' nLevel=0') + " mun='" + sMun +"'></option>")
 				.attr("value", sValue)
 				.text(sValue));
 		}
@@ -73,7 +71,7 @@ define(['jquery', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/selec
 	};
 
 	CustomSelect2.prototype.getParameters = function () {
-		var sValue = $('.js-example-basic-single').(':selected').attr('mun');
+		var sValue = $('.js-example-basic-single').find(':selected').attr('mun');
 		return [{
 				"parameter": sParamName.toString(),
 				"values": [{
