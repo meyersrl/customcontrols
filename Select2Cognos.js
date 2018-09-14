4f7c2dcc1ec10f533b78a6c357f80b8f16fb1b1b
meyersrl <36867565+meyersrl@users.noreply.github.com>
2018-09-13 23:24:30 -0700

    Updated for multiple controls

--- Select2Cognos.js
+++ Select2Cognos.js
@@ -24,12 +24,15 @@ function formatElement(element) {
 
 var munDataSet;
 var sParamName = '';
+var sIdentifier = '';
+var nKey;
 
 define(['jquery', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js'], function ($) {
 
 	function CustomSelect2() {};
 
 	CustomSelect2.prototype.draw = function (oControlHost) {
+		
 		sParamName = oControlHost.configuration['paramName'];
 		sWidth = oControlHost.configuration['width'];
 		var oParameter = oControlHost.getParameter(sParamName.toString());
@@ -39,7 +42,8 @@ define(['jquery', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/selec
 		munDataSet = oControlHost.control.dataStores[2];
 		var iRowCount = nameDataSet.rowCount;
 		var iPriorLevel = levelDataSet.getCellValue(0, 0);
-		var sHTML = '<select class="js-example-basic-single" style="width:' + sWidth + ';" ></select>';
+		sIdentifier = sParamName + '_Select2';
+		var sHTML = '<select class="'+ sIdentifier +'"style="width:' + sWidth + ';" ></select>';
 		var el = oControlHost.container;
 		$(el).append(sHTML);
 
@@ -49,32 +53,32 @@ define(['jquery', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/selec
 			var sMUN = munDataSet.getCellValue( iRow, 0);
 			var nNextLevel = levelDataSet.getCellValue(((iRow < iRowCount - 1) ? iRow + 1 : iRow), 0);
 			//console.log('Current Element:' + sValue + ' Current Level:' + nLevel + ' Next Level:' + nNextLevel);
-			$('.js-example-basic-single')
+			$('.' + sIdentifier)
 			.append($("<option level=" + nLevel + ((sMUN == sParameterValue) ? ' selected="selected"' : '') + ((nLevel < nNextLevel) ? ' nLevel=1' : ' nLevel=0') + " key=" + iRow +"></option>")
 				.attr("value", sValue)
 				.text(sValue));
 		}
 
-		$('.js-example-basic-single').on('select2:select', function () {
+		$('.' + sIdentifier).on('select2:select', function (event) {
+			nKey = parseInt($(event.currentTarget).find("option:selected").attr('key'));
 			oControlHost.valueChanged();
 			oControlHost.next();
 		});
 
-		$('.js-example-basic-single').select2({
+		$('.' + sIdentifier).select2({
 			templateResult: formatElement
 		});
-
 	}
 
 	CustomSelect2.prototype.setData = function (oControlHost, oDataStore) {
 		this.m_oDataStore = oDataStore;
 	};
 
-	CustomSelect2.prototype.getParameters = function () {
-		var nKey = parseInt($('.js-example-basic-single').find(':selected').attr('key'));
-		var sValue = munDataSet.getCellValue(nKey, 0);
+	CustomSelect2.prototype.getParameters = function (oControlHost) {
+		var sParamName = oControlHost.configuration['paramName'];
+		var sValue = oControlHost.control.dataStores[2].getCellValue(nKey, 0);
 		return [{
-				"parameter": sParamName.toString(),
+				"parameter": sParamName.toString(),	
 				"values": [{
 						"use": sValue
 					}
