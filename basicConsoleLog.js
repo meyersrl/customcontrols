e0c3b2ea960dfc07a252e4d4ff650da96ad9a0b6
meyersrl <36867565+meyersrl@users.noreply.github.com>
2018-09-25 15:51:07 -0700

    basicTest

---
+++ basicConsoleLog.js
@@ -0,0 +1,15 @@
+define( 
+  [   "jquery"
+    
+  ]
+  , function(jQuery) {
+    "use strict"; 
+
+    function expandCollapse(){};
+
+	console.log('Javascript ran successfully!');
+    
+
+    return expandCollapse;
+  }
+);
